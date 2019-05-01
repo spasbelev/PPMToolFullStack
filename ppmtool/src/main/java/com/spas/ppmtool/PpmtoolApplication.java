f59a628a8cfd56061b455bddfb365d69ecad734b
Spas Belev <spasbelev@gmail.com>
2019-05-01 21:22:43 +0300

    Add project service/controller and repository

--- ppmtool/src/main/java/com/spas/ppmtool/PpmtoolApplication.java
+++ ppmtool/src/main/java/com/spas/ppmtool/PpmtoolApplication.java
@@ -2,12 +2,13 @@ package com.spas.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.spas")
 @SpringBootApplication
 public class PpmtoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PpmtoolApplication.class, args);
 	}
-
 }
